a59c0044069cba9930a9d78d81c0aad4e35be5cc
DongHyun Kim <developerforus@gmail.com>
2022-06-03 17:46:52 +0900

    jest

--- jest2/app.test.js
+++ jest2/app.test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("./app");
 
+// basic?
 describe("Test the root path", () => {
   test("It should response the GET method", (done) => {
     request(app)
@@ -9,5 +10,24 @@ describe("Test the root path", () => {
         expect(response.statusCode).toBe(200);
         done();
       });
+  });
+});
+
+// done 없어도 됨!
+describe("Test the root path", () => {
+  test("It should response the GET method", () => {
+    return request(app)
+      .get("/")
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+      });
+  });
+});
+
+// async await
+describe("Test the root path", () => {
+  test("It should response the GET method", async () => {
+    const response = await request(app).get("/");
+    expect(response.statusCode).toBe(200);
   });
 });
