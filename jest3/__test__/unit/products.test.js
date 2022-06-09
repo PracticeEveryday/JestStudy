4b27880e12a589dffa7fec99dc09ed526aa99070
DongHyun Kim <developerforus@gmail.com>
2022-06-10 07:19:11 +0900

    [Create]: Jest CodeStatus Check

--- jest3/__test__/unit/products.test.js
+++ jest3/__test__/unit/products.test.js
@@ -1,28 +1,46 @@
 const productController = require("../../controller/products");
 const productModel = require("../../models/Product");
+
 const httpMocks = require("node-mocks-http");
 const newProduct = require("../data/new-product.json");
 
-// mock함수
+// mock 함수
 productModel.create = jest.fn();
 
-describe("Product Controller Create", () => {
-  it("should have a createProduct function", () => {
+let req, res, next;
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
+describe("프로덕트 컨트롤러 Create 함수", () => {
+  beforeEach(() => {
+    req.body = newProduct;
+  });
+
+  it("createProduct 함수일 것이다.", () => {
     // ProductController에 createProduct가 함수인지 파악하는것
     // 예상을 하면서 만드는것
     expect(typeof productController.createProduct).toBe("function");
   });
-  it("should call ProductModel.create", () => {
-    const req = httpMocks.createRequest();
-    const res = httpMocks.createResponse();
-    const next = null;
+
+  it("프로덕트 컨트롤러의 크리에이트프로덕트 함수를 호출하면 프로덕트 모델의 크리에읕 함수가 호출 될 것이다.", () => {
     // req.body에 newProduct를 넣어준다.
     req.body = newProduct;
     productController.createProduct(req, res, next);
-    // productController의 createProduct가 실행될때
-    // productModel의 create가 호출되는지 확인
-    // DB에 직접적인 영향을 받으면 안되기 때문에
-    // mock함수인 jest.fn()을 확인한다.
+    // productController의 createProduct가 실행될 때
+    // productModel의 create가 호출 되는지 확인한다.
+    // DB에 직접적은 영향을 주면 안되기 때문에
+    // mock 함수인 jest.fn()을 사용하여 확인한다.
     expect(productModel.create).toBeCalledWith(newProduct);
+  });
+
+  // data를 성공적으로 create시 201 반환
+  it("201 응답 코드를 반환할 것임", () => {
+    productController.createProduct(req, res, next);
+    expect(res.statusCode).toBe(201);
+    expect(res._isEndCalled()).toBeTruthy();
   });
 });
--- jest3/controller/products.js
+++ jest3/controller/products.js
@@ -2,6 +2,7 @@ const productModel = require("../models/Product");
 
 const createProduct = (req, res, next) => {
   productModel.create(req.body);
+  res.status(201).send();
 };
 
 module.exports = { createProduct };
