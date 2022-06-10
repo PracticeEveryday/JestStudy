32455f8e25cbd1115031c55fb7a125204d9b5d48
DongHyun Kim <developerforus@gmail.com>
2022-06-10 15:53:25 +0900

    [Test]: Jest Context

---
+++ jest4/dist/add.test.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const add_1 = require("./add");
+describe("plus", () => {
+    it("1+1 = 2", () => {
+        expect((0, add_1.add)(1, 1)).toBe(2);
+    });
+});
+//# sourceMappingURL=add.test.js.map
