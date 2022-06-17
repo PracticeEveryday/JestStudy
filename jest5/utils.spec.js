9562f5cc0c579bfa96729464b433413484e77c1f
DongHyun Kim <developerforus@gmail.com>
2022-06-17 13:32:29 +0900

    Unit test

--- jest5/utils.spec.js
+++ jest5/utils.spec.js
@@ -1,6 +1,7 @@
 const utils = require("./utils");
 const should = require("should");
 
+// 단위 테스트 하나의 함수만 테스트 하는 것
 describe("utils.js 모듈의 capitalize() 함수는", () => {
   it("문자열의 첫번째 문자를 대문자로 변환한다.", () => {
     const result = utils.capitalize("hello");
