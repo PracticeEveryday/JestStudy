683aab05dcb3c3c473c0c715de22fe6f9597480b
DongHyun Kim <developerforus@gmail.com>
2022-06-17 19:45:03 +0900

    API Test ID

--- jest5/index.js
+++ jest5/index.js
@@ -1,14 +1,37 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const fs = require("fs");
 
-const user = fs.readFileSync("./data.txt", "utf8");
+const users = [
+  { id: 1, name: "kim" },
+  { id: 2, name: "lee" },
+  { id: 3, name: "park" },
+];
 
 app.use(morgan("dev"));
 
 app.get("/users", (req, res) => {
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10); // "2"
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
+  res.json(users.slice(0, limit));
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  if (Number.isNaN(id)) return res.status(400).end();
+
+  const user = users.filter((user) => user.id === id)[0];
+  if (!user) return res.status(404).end();
+
   res.json(user);
+});
+app.delete("/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  users.filter((user) => user.id !== id);
+  res.status(204).end();
 });
 
 app.listen(3000, () => console.log("3000번 포트 온"));
--- jest5/index.spec.js
+++ jest5/index.spec.js
@@ -1,15 +1,62 @@
 const app = require("./index");
 const request = require("supertest");
+const should = require("should");
 
 describe("GET/ users는,", () => {
   // 우리가 만든 api 서버는 비동기도 돌아가기에 비동기 처리를 해야함
   // 함수 파라미터로 done 콜백 파라미터 넣어주고 테스트가 끝날때 done 콜백 함수 호출!
-  it("...", (done) => {
-    request(app)
-      .get("/users")
-      .end((err, res) => {
-        console.log(res.body);
-        done();
+  describe("성공 시, ", () => {
+    it("유저 객체를 담은 배열로 응답한다.", (done) => {
+      request(app)
+        .get("/users")
+        .end((err, res) => {
+          res.body.should.be.instanceof(Array);
+          done();
+        });
+    });
+    it("최대 limit 갯수만큼 응답한다.", (done) => {
+      request(app)
+        .get("/users?limit=2")
+        .end((err, res) => {
+          res.body.should.have.lengthOf(2);
+          done();
+        });
+    });
+
+    describe("실패 시", () => {
+      it("limit이 숫자형이 아니면 400을 응답한다.", (done) => {
+        request(app).get("/users?limit=two").expect(400).end(done);
       });
+    });
+  });
+});
+
+describe("GET/users/1는 ", () => {
+  describe("성공시", () => {
+    it("id가 1인 유저 객체를 반환한다.", (done) => {
+      request(app)
+        .get("/users/1")
+        .end((err, res) => {
+          res.body.should.have.property("id", 1);
+          console.log(res.body);
+          done();
+        });
+    });
+  });
+  describe("실피새", () => {
+    it("id가 숫자가 아닐 경우 400으로 응답한다.", (done) => {
+      request(app).get("/users/one").expect(400).end(done);
+    });
+    it("id로 유저를 찾을 수 없을 경우 404로 응답한다.", (done) => {
+      request(app).get("/users/999").expect(404).end(done);
+    });
+  });
+});
+
+describe("Delete/users/1", () => {
+  describe("성공시", () => {
+    it("204를 응답한다.", (done) => {
+      request(app).delete("/users/1").expect(204).end(done);
+    });
   });
 });
