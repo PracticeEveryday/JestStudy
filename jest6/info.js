6b1bba9253dae71770414e34bc9a135cb8db7f47
DongHyun Kim <developerforus@gmail.com>
2022-06-28 21:31:15 +0900

    info

---
+++ jest6/info.js
@@ -0,0 +1,22 @@
+// express => node_modules 안에 npm으로 추가한 외부 모듈이 추가됨.
+
+// git => node_modules 저장 x
+// git clone시 바로 사용할 수 없음.
+// npm init => 프로젝트 초기화
+// package.json 정보 추가!
+
+// http 요청
+// 모든 자원은 명사로 식별한다.
+// HTTP 경로로 자원을 요청한다.
+
+// GET /users
+// GET /users/{id}
+
+// http method
+// 서버 자원에 대한 행동을 나타냄
+// GET 자원을 조회
+// POST 자원을 생성
+// PUT 자원을 갱신
+// DELETE 자원을 삭제
+
+// 이는 익스프레스 app의 메소드로 구현되어 있음.
