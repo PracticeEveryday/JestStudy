9522012816f630da7d207a9ca7d6fc1374d244d3
DongHyun Kim <developerforus@gmail.com>
2022-06-29 14:28:18 +0900

    first test code

--- jest6/index.spec.js
+++ jest6/index.spec.js
@@ -4,14 +4,17 @@ const app = require("./index");
 
 // 보통 http 이름을 써줌
 describe("GET users/", () => {
-  it("성공 시 유저 객체를 담은 배열로 응답한다.", (done) => {
-    // 우리가 만든 api 서버는 모두 비동기로 동작하게 됨!!
-    // 비동기 처리 해주기 it 2번째 파라미터의 함수에 done이란 콜백 함수를 넣어주고 테스트가 끝나는 시점에 done콜백 함수 호출해주면 끝
-    request(app)
-      .get("/users")
-      .end((err, res) => {
-        console.log(res.body);
-        done();
-      });
+  describe("성공 시 ", () => {
+    it("유저 객체를 담은 배열로 응답한다.", (done) => {
+      // 우리가 만든 api 서버는 모두 비동기로 동작하게 됨!!
+      // 비동기 처리 해주기 it 2번째 파라미터의 함수에 done이란 콜백 함수를 넣어주고 테스트가 끝나는 시점에 done콜백 함수 호출해주면 끝
+      request(app)
+        .get("/users")
+        .end((err, res) => {
+          //console.log(res.body);
+          res.body.should.be.instanceof(Array);
+          done();
+        });
+    });
   });
 });
