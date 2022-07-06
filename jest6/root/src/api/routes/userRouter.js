95d55ce7dd5c5700abbd425f245a38fba870fda6
DongHyun Kim <developerforus@gmail.com>
2022-07-06 10:57:39 +0900

    Service division

--- jest6/root/src/api/routes/userRouter.js
+++ jest6/root/src/api/routes/userRouter.js
@@ -1,59 +1,19 @@
 import { Router } from "express";
-
-import { users } from "../../db";
+import { UserService } from "../../services/userService";
 
 export default (app) => {
   const userRouter = Router();
 
+  const userService = new UserService();
+
   app.use("/users", userRouter);
 
-  userRouter.get("/", (req, res) => {
-    req.query.limit = req.query.limit || 10;
-    const limit = parseInt(req.query.limit, 10);
-
-    if (Number.isNaN(limit)) {
-      // return을 안 넣어주면 아래 코드 다 실행 되서 중복 ㅠ
-      return res.status(400).end();
-    }
-    res.json(users.slice(0, limit));
-  });
+  userRouter.get("/", userService.finds);
 
   // get users/:id 라우터 추가!
-  userRouter.get("/:id", (req, res) => {
-    const id = parseInt(req.params.id, 10);
+  userRouter.get("/:id", userService.findOne);
 
-    if (Number.isNaN(id)) {
-      return res.status(400).end();
-    }
-    const user = users.filter((item) => item.id === id)[0];
+  userRouter.delete("/:id", userService.deleteUser);
 
-    if (!user) {
-      return res.status(404).end();
-    }
-    res.status(201).json(user);
-  });
-
-  userRouter.delete("/:id", (req, res) => {
-    const id = parseInt(req.params.id, 10);
-
-    if (Number.isNaN(id)) {
-      return res.status(400).end();
-    }
-    const user = users.filter((item) => item.id !== id);
-    res.status(204).json(user);
-  });
-
-  userRouter.post("/", (req, res) => {
-    const name = req.body.name;
-    if (!name) {
-      return res.status(400).end();
-    } else if (users.filter((item) => item.name === name).length) {
-      return res.status(409).end();
-    }
-    const id = Date.now();
-    const user = { id, name };
-    users.push(user);
-    console.log(user);
-    res.status(201).json(user);
-  });
+  userRouter.post("/", userService.addUser);
 };
---
+++ jest6/root/src/services/userService.js
@@ -0,0 +1,52 @@
+import { users } from "../db";
+
+export class UserService {
+  finds(req, res) {
+    req.query.limit = req.query.limit || 10;
+    const limit = parseInt(req.query.limit, 10);
+
+    if (Number.isNaN(limit)) {
+      // return을 안 넣어주면 아래 코드 다 실행 되서 중복 ㅠ
+      return res.status(400).end();
+    }
+    res.json(users.slice(0, limit));
+  }
+
+  findOne(req, res) {
+    const id = parseInt(req.params.id, 10);
+
+    if (Number.isNaN(id)) {
+      return res.status(400).end();
+    }
+    const user = users.filter((item) => item.id === id)[0];
+
+    if (!user) {
+      return res.status(404).end();
+    }
+    res.status(201).json(user);
+  }
+
+  deleteUser(req, res) {
+    const id = parseInt(req.params.id, 10);
+
+    if (Number.isNaN(id)) {
+      return res.status(400).end();
+    }
+    const user = users.filter((item) => item.id !== id);
+    res.status(204).json(user);
+  }
+
+  addUser(req, res) {
+    const name = req.body.name;
+    if (!name) {
+      return res.status(400).end();
+    } else if (users.filter((item) => item.name === name).length) {
+      return res.status(409).end();
+    }
+    const id = Date.now();
+    const user = { id, name };
+    users.push(user);
+    console.log(user);
+    res.status(201).json(user);
+  }
+}
