6170a33563a1b2df07a8bf33b93441f60550e513
DongHyun Kim <developerforus@gmail.com>
2022-07-06 11:15:44 +0900

    test folder division

--- jest6/root/__test__/index.spec.js
+++ jest6/root/__test__/index.spec.js
@@ -1,6 +1,6 @@
 import request from "supertest";
 import should from "should";
-import { app } from "./src/app";
+import { app } from "../src/app";
 // request(app) 은 서버를 구동함 하지만 index.js에도 서버를 고동하는 코드가 있음
 /* app.listen(3000, console.log("3000번 포트 온")); */ // => 서버 구동이 중복으로 일어남
 // so index로 분리
